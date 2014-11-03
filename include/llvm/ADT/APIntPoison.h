d802324332513fe92cab91393d2be28841a1dab2
Christian Schreiner <cas@cs.utah.edu>
2014-11-03 13:54:04 -0700

    added poison-checking functions for all the operations that need it

--- include/llvm/ADT/APIntPoison.h
+++ include/llvm/ADT/APIntPoison.h
@@ -24,42 +24,28 @@
 
 namespace llvm {
 
-// TODO: fill this in for APIntPoison
 //===----------------------------------------------------------------------===//
-//                              APInt Class
-//===----------------------------------------------------------------------===//
-
-/// \brief Class for arbitrary precision integers.
-///
-/// APInt is a functional replacement for common case unsigned integer type like
-/// "unsigned", "unsigned long" or "uint64_t", but also allows non-byte-width
-/// integer sizes and large integer value types such as 3-bits, 15-bits, or more
-/// than 64-bits of precision. APInt provides a variety of arithmetic operators
-/// and methods to manipulate integer values of any bit-width. It supports both
-/// the typical integer arithmetic and comparison operations as well as bitwise
-/// manipulation.
-///
-/// The class has several invariants worth noting:
-///   * All bit, byte, and word positions are zero-based.
-///   * Once the bit width is set, it doesn't change except by the Truncate,
-///     SignExtend, or ZeroExtend operations.
-///   * All binary operators must be on APInt instances of the same bit width.
-///     Attempting to use these operators on instances with different bit
-///     widths will yield an assertion.
-///   * The value is stored canonically as an unsigned value. For operations
-///     where it makes a difference, there are both signed and unsigned variants
-///     of the operation. For example, sdiv and udiv. However, because the bit
-///     widths must be the same, operations such as Mul and Add produce the same
-///     results regardless of whether the values are interpreted as signed or
-///     not.
-///   * In general, the class tries to follow the style of computation that LLVM
-///     uses in its IR. This simplifies its use for LLVM.
-///
+//                              APIntPoison Namespace
 namespace APIntPoison {
 
 poisonIfNeeded_uadd( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
 poisonIfNeeded_sadd( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
-/* TODO2: add other methods for other operations */
+poisonIfNeeded_usub( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_ssub( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_umul( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_smul( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_udiv( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_sdiv( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+
+poisonIfNeeded_ushl( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_sshl( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_ulshr( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_slshr( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_uashr( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+poisonIfNeeded_sashr( APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
+
+poisonIfNeeded_getelementptr( 
+    APInt& dest, APInt& LHS, APInt& RHS, boolean noWrap );
 
 } // End of APIntPoison namespace
 
