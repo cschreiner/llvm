418f8d1782e5e8ab66179852912d3808dcd58c3e
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:26:29 -0700

    added initial working version of code

--- test/lli_undef_fix/random.cpp
+++ test/lli_undef_fix/random.cpp
@@ -1,8 +1,10 @@
-//#include <APInt.h>
+#include <APInt.h>
 #include <iostream>
 #include <ostream>
 
 int main ( int argc, char* argv[] )
-{
-  std::cout << "Hello, world. \n";
-}
+{{
+  std::cout << "generating random value... \n";
+  APInt george= llvm::APInt::getRandom( 32, false );
+  std::cout << "val=\"" << george.toString( 10, false ) << "\"\n";
+}}
