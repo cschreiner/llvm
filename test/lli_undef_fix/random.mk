c4736c5c49ecce9d3a9b937328b147a7b0a0822e
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:02:26 -0700

    rearranged to follow advice on another webpage, sheesh

--- test/lli_undef_fix/random.mk
+++ test/lli_undef_fix/random.mk
@@ -1,7 +1,10 @@
 
 
 random: random.cpp
-	clang++ `llvm-config --cxxflags` -c random.cpp
-	clang++ `llvm-config --ldflags` \
-	    -stdlib=libc++ \
-	    `llvm-config --libs` -o random random.o
+	clang++ -stdlib=libc++ random.cpp -nodefaultlibs -lc++ -lc++abi -lm \
+	    -lc -lgcc_s -lgcc
+
+#	clang++ `llvm-config --cxxflags` -c random.cpp
+#	clang++ `llvm-config --ldflags` \
+#	    -stdlib=libc++ \
+#	    `llvm-config --libs` -o random random.o
