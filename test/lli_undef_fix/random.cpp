e30e60d6eef7c9c140855125c33d467b8f468632
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:28:33 -0700

    fixed namespacing

--- test/lli_undef_fix/random.cpp
+++ test/lli_undef_fix/random.cpp
@@ -1,10 +1,10 @@
-#include <APInt.h>
+#include "llvm/ADT/APInt.h"
 #include <iostream>
 #include <ostream>
 
 int main ( int argc, char* argv[] )
 {{
   std::cout << "generating random value... \n";
-  APInt george= llvm::APInt::getRandom( 32, false );
+  llvm::APInt george= llvm::APInt::getRandom( 32, false );
   std::cout << "val=\"" << george.toString( 10, false ) << "\"\n";
 }}
