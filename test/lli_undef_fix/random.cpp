ea884c60f525bcf3ff1abdb57b5d699ba18911f3
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:52:03 -0700

    expanded to print out random numbers of various sizes

--- test/lli_undef_fix/random.cpp
+++ test/lli_undef_fix/random.cpp
@@ -1,10 +1,19 @@
 #include "llvm/ADT/APInt.h"
 #include <iostream>
 #include <ostream>
+#include <cstdlib>
 
 int main ( int argc, char* argv[] )
 {{
-  std::cout << "generating random value... \n";
-  llvm::APInt george= llvm::APInt::getRandom( 32, false );
-  std::cout << "val=\"" << george.toString( 10, false ) << "\"\n";
+  int bitsizes[]= { 32, 16, 48, 64, 72, -1 };
+  int ii;
+  llvm::APInt george;
+
+  srand(45);
+
+  for ( ii= 0; bitsizes[ii] != -1; ii++ )  {
+    std::cout << "generating random value of " << bitsizes[ii] << "bits... \n";
+    george= llvm::APInt::getRandom( bitsizes[ii], false );
+    std::cout << "   val=\"" << george.toString( 10, false ) << "\"\n";
+  }
 }}
