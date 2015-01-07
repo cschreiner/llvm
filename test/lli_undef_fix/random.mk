0b02584ef2c90ab2f17d781fef6b311802ebf6ba
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:23:12 -0700

    this builds, too

--- test/lli_undef_fix/random.mk
+++ test/lli_undef_fix/random.mk
@@ -1,8 +1,9 @@
 
 
 random: random.cpp
-	g++ \
-	    -std=c++11 random.cpp -o random
+	g++ -std=c++11 \
+	    `llvm-config --cxxflags --ldflags --libs ` \
+	    random.cpp -o random
 
 #	clang++ `llvm-config --cxxflags` -c random.cpp
 #	clang++ `llvm-config --ldflags` \
