e65076b803cd0d06203138e349fef99a0280225e
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 20:03:09 -0700

    changed to produce multiple random numbers of each bitlength, so we can test that multiple numbers per bitlength are possible

--- test/lli_undef_fix/random.cpp
+++ test/lli_undef_fix/random.cpp
@@ -6,14 +6,17 @@
 int main ( int argc, char* argv[] )
 {{
   int bitsizes[]= { 32, 16, 48, 64, 72, -1 };
-  int ii;
+  int ii, jj;
   llvm::APInt george;
 
   srand(45);
 
   for ( ii= 0; bitsizes[ii] != -1; ii++ )  {
     std::cout << "generating random value of " << bitsizes[ii] << "bits... \n";
-    george= llvm::APInt::getRandom( bitsizes[ii], false );
-    std::cout << "   val=\"" << george.toString( 10, false ) << "\"\n";
+    for ( jj= 0; jj < 3; jj++ )  {
+      george= llvm::APInt::getRandom( bitsizes[ii], false );
+      std::cout << "   val #" << jj << "=\"" << 
+	  george.toString( 10, false ) << "\"\n";
+    }
   }
 }}
