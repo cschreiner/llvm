07246cefebaef491f7e3dab48903a1cfcf637621
Christian Schreiner <cas@cs.utah.edu>
2015-01-06 19:47:19 -0700

    added a missing parameter to llvm-config

--- test/lli_undef_fix/random.mk
+++ test/lli_undef_fix/random.mk
@@ -1,9 +1,15 @@
 
 
 random: random.cpp
-	g++ -std=c++11 \
-	    `llvm-config --cxxflags --ldflags --libs ` \
-	    random.cpp -o random
+	g++ -std=c++11 `llvm-config --cxxflags` -c random.cpp
+	g++ `llvm-config --ldflags --system-libs` \
+	    random.o \
+	    `llvm-config --libs` -lpthread \
+	    -o random 
+
+#	g++ -std=c++11 \
+#	    `llvm-config --cxxflags --ldflags --libs ` \
+#	    random.cpp -o random
 
 #	clang++ `llvm-config --cxxflags` -c random.cpp
 #	clang++ `llvm-config --ldflags` \
