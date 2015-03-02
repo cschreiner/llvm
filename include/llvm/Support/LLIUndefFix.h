37626c7b6e3d6ebb13b4bcd84b0080c56474f436
Christian Schreiner (home) <cas@mathoni.net>
2015-03-02 10:04:02 -0700

    (hopefully) made a variable name more groupable when it is alphabetized with expected future additions

--- include/llvm/Support/LLIUndefFix.h
+++ include/llvm/Support/LLIUndefFix.h
@@ -42,7 +42,7 @@ namespace lli_undef_fix {
    * **************************************************************************
    */
 
-cl::opt<bool> arg_luf_select_antidote;
+cl::opt<bool> arg_luf_antidote_select;
 
 
 
