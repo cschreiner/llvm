efa58ed7ee5ec86c27fbc13abebf6a3689b9a8d7
Christian Schreiner (home) <cas@mathoni.net>
2015-03-02 14:30:59 -0700

    that last version compiled

--- lib/ExecutionEngine/Interpreter/Execution.cpp
+++ lib/ExecutionEngine/Interpreter/Execution.cpp
@@ -920,7 +920,7 @@ static GenericValue executeSelectInst(GenericValue Src1, GenericValue Src2,
 	    Src3.IntVal.getPoisoned() : Src2.IntVal.getPoisoned() 
 	  );
     }
-    #if 0 //;;
+    #if 0 //;; 
     if ( Src1.IntVal.getPoisoned() || Src2.IntVal.getPoisoned() || 
 	  Src3.IntVal.getPoisoned() || Dest.IntVal.getPoisoned() )  {
        std::cout << "   select: Src1=" << Src1.IntVal.getPoisoned() <<
