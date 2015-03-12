98339a23903709f455960fd7d4c6868f46d26daa
Christian Schreiner (home) <cas@mathoni.net>
2015-03-11 23:35:05 -0600

    adding file templates for code to formalize various options

---
+++ include/llvm/Support/LUF_opts.h
@@ -0,0 +1,69 @@
+/*** 
+   * Program Name: lli_undef_fix
+   *
+   * File Name: LUF_opts.h
+   *
+   * File Description: 
+   *
+   * lli_undef_fix was written by Christian A. Schreiner at University of
+   * Utah.  Copyright (C) 2015-2015 by University of Utah.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If you
+   * use it and something breaks, you are solely responsible for cleaning up
+   * afterwards.
+   *
+   */
+
+/*** Configuration Management Information:
+   * 
+   * $Source: /home/cas/templates/auto_uut/RCS/cas_tmpl.h,v $
+   * $Author: cas $
+   * $Date: 2014/05/02 19:51:11 $
+   * $Revision: 1.4 $
+   * 
+   */
+
+#if !defined(__LUF_OPTS_H__)
+#define __LUF_OPTS_H__
+
+/*** **************************************************************************
+   *   includes
+   * **************************************************************************
+   */
+
+/*** **************************************************************************
+   *   declarations
+   * **************************************************************************
+   */
+
+/*** ==========================================================================
+   *   function prototypes
+   */
+
+// template is 17 lines long
+/*** --------------------------------------------------------------------------
+   * function name()
+   * --------------------------------------------------------------------------
+   * Description: 
+   *
+   * Method: 
+   *
+   * Reentrancy: 
+   *
+   * Inputs: 
+   *    
+   * Outputs: 
+   *
+   * Return Value: 
+   *
+   */
+//void name()
+
+
+/*** **************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+#endif /* !defined(__LUF_OPTS_H__) */
+
---
+++ lib/Support/LUF_opts.cpp
@@ -0,0 +1,82 @@
+/*** 
+   * Program Name: lli_undef_fix 
+   *
+   * \file LUF_opts.cpp 
+   *
+   * \brief 
+   *
+   * \b Detailed_Description: 
+   *
+   * <$project_name) was written by Christian A. Schreiner at University of
+   * Utah.  Copyright (C) 2015-2015 by University of Utah.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If you
+   * use it and something breaks, you are solely responsible for cleaning up
+   * afterwards.
+   */
+
+/*++ Configuration Management Information:
+   * 
+   * $Source: /net/home/cas/templates/auto_uut/RCS/cas_tmpl.cpp,v $
+   * $File: $
+   * $Author: cas $
+   * $Date: 2015/02/28 04:15:43 $
+   * $Revision: 1.4 $
+   * 
+   */
+
+/*++ **************************************************************************
+   *   includes
+   * **************************************************************************
+   */
+
+/*++ **************************************************************************
+   *   declarations
+   * **************************************************************************
+   */
+
+/*++ ==========================================================================
+   *   un-submoduled function prototypes
+   * ==========================================================================
+   */
+
+/*++ **************************************************************************
+   *   source code
+   * **************************************************************************
+   */
+
+// template is 22 lines long
+// ----------------------------------------------------------------------------
+///  \fn name()
+// ----------------------------------------------------------------------------
+/*** \brief 
+   *
+   * \b Detailed_Description: 
+   *
+   * \b Method: 
+   *
+   * \b Reentrancy: 
+   *
+   * \param xx (input) 
+   *    
+   * \param yy (output) 
+   *
+   * \return 
+   *
+   */
+//void name()
+//{{
+//}}
+
+
+/*++ **************************************************************************
+   *   closing
+   * **************************************************************************
+   */
+
+/*++ **************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+
