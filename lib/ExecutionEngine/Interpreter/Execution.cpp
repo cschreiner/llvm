e3a2b7d8dae1df0ad1653f2f4701e6b2d260bbd2
Christian Schreiner (home) <cas@mathoni.net>
2015-03-12 01:21:25 -0600

    tweaked debug code

--- lib/ExecutionEngine/Interpreter/Execution.cpp
+++ lib/ExecutionEngine/Interpreter/Execution.cpp
@@ -852,7 +852,8 @@ void Interpreter::visitBinaryOperator(BinaryOperator &I) {
       APIntPoison::poisonIfNeeded_sub( R.IntVal, Src1.IntVal, Src2.IntVal, 
           I.hasNoSignedWrap(), I.hasNoUnsignedWrap() );
       APIntPoison::printIfPoison( I, R.IntVal );
-      if ( R.IntVal.getPoisoned() || true )  { //;;
+      //;;if ( R.IntVal.getPoisoned() )  { //;;
+      if ( true )  { //;;
         std::cout << 
 	    "\t" "Src1  = " << Src1.IntVal.toString() << "\n" <<
 	    "\t" "Src2  = " << Src2.IntVal.toString() << "\n" <<
