95c82bb5634315a570639e009441e3fd124fa958
Christian Schreiner <cas@cs.utah.edu>
2015-03-12 21:12:15 -0600

    adding a file for miscelaneous lli_undef_fix (LUF) stuff

---
+++ include/llvm/Support/LUF_etc.h
@@ -0,0 +1,48 @@
+/*** 
+   * Program Name: <$project_name>
+   *
+   * File Name: LUF_etc.h
+   *
+   * File Description: 
+   *
+   * Copyright (C) 2015-2015 by Christian A. Schreiner.  All rights
+   * reserved.  You may use, examine, or modify this file only in accordance
+   * with the GNU Public License, or, alternately, by special written
+   * arrangement with the author.  This file comes with no warranties.  If you
+   * use it and something breaks, you are solely responsible for cleaning up
+   * afterwards.
+   *
+   */
+
+/*** Configuration Management Information:
+   * 
+   * $Source: /net/home/cas/templates/auto/RCS/cas_tmpl.h,v $
+   * $Author: cas $
+   * $Date: 2013/02/04 15:57:01 $
+   * $Revision: 1.4 $
+   * 
+   */
+
+#if !defined(__LUF_ETC_H__)
+#define __LUF_ETC_H__
+
+/*** **************************************************************************
+   *   includes
+   * **************************************************************************
+   */
+
+/*** **************************************************************************
+   *   declarations
+   * **************************************************************************
+   */
+
+/*** ==========================================================================
+   *   function prototypes
+   */
+
+/*** **************************************************************************
+   *   end of file
+   * **************************************************************************
+   */
+#endif /* !defined(__LUF_ETC_H__) */
+
